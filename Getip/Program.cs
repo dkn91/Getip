7e9a2fbfbaf78b5cb04e261544ef6bde42b6c006
dpishe <deepakpishe@outlook.com>
2016-04-19 17:27:02 -0700

    gatewayip

--- Getip/Program.cs
+++ Getip/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,16 @@ namespace Getip
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var card = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault();
+            //Console.WriteLine(NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault().Name.ToString());
+
+            string address = card.GetIPProperties().GatewayAddresses.FirstOrDefault().Address.ToString();
+            
+            Console.WriteLine(address );
+            Console.ReadLine();
         }
+
     }
 }
